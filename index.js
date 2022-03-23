910faee534202d5ceec7f37b248e3bb27c87f3e4
Simon | @ <88943961+Alchemist-developer@users.noreply.github.com>
2022-03-22 22:15:29 -0300

    new update

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const empresa = "Sistema Omma";
-console.log(`Seja bem vindo ao ${empresa}`);
+  const empresa = "Sistema Omma";
+  console.log(`Seja bem vindo ao ${empresa}`);
 
-const listaDeReceitas = [{
-  id: 1,
-  titulo: "Cachorro Quente",
-  dificuldade: "simples",
-  ingredientes: ["1 pao de leite", "1 Salsicha", "Maionese"],
-  preparo: "assim e assado",
-  link: "youtube.com",
-  vegano: false,
+  const listaDeReceitas = [{
+    id: 1,
+    titulo: "Cachorro Quente",
+    dificuldade: "simples",
+    ingredientes: ["1 pao de leite", "1 Salsicha", "Maionese"],
+    preparo: "jodbvnujd osbvcujsbn jsbfusb",
+    link: "youtube.com",
+    vegano: false,
 }, ];
 
-const cadastrarReceita = (
-  id,
-  titulo,
-  dificuldade,
-  ingredientes,
-  preparo,
-  link,
-  vegano
+  const cadastrarReceita = (
+    id,
+    titulo,
+    dificuldade,
+    ingredientes,
+    preparo,
+    link,
+    vegano
 ) => {
   const novaReceita = {
     id,
@@ -34,93 +34,104 @@ const cadastrarReceita = (
   console.log(`Cadastro da receita ${titulo} feito com sucesso!`);
 };
 
-const btnEnviar = document.querySelector('#btnEnviar');
+  const btnEnviar = document.querySelector('#btnEnviar');
 
-btnEnviar.onclick = (evento) => {
-  // impede que a página seja recarregada
-  evento.preventDefault();
+  btnEnviar.onclick = (evento) => {
+    // impede que a pagina seja recarregada / previne comportamento padrão 
+    evento.preventDefault();
+
+    alert('formulario enviado');
+
+    let inputTitulo = document.querySelector('#titulo');
+    let inputDificuldade = document.querySelector('#dificuldade');
+    let inputIngredientes = document.querySelector('#ingredientes');
+    let inputPreparo = document.querySelector('#preparo');
+    let inputlink = document.querySelector('#link');
+    let inputvegano = document.querySelector('input[name="vegano"]:checked');
   
-  // alert ('formulario enviado!');
-  let inputTitulo = document.querySelector('#titulo');
-  let inputDificuldade = document.querySelector('#dificuldade');
-  let inputIngredientes = document.querySelector('#ingredientes');
-  let inputPreparo = document.querySelector('#preparo');
-  let inputLink = document.querySelector('#link');
-  let inputVegano = document.querySelector('input[name="vegano"]:checked');
-
-  let novaReceita = {
-    id: listaDeReceitas.length,
-    titulo: inputTitulo.value,
-    dificuldade: inputDificuldade.value,
-    ingredientes: inputIngredientes.value.split(','),
-    preparo: inputPreparo.value,
-    link: inputLink.value,
-    // vegano: inputVegano.value
-
+    let novaReceita = {
+      id: listaDeReceitas.length,
+      titulo: inputTitulo.value,
+      dificuldade: inputDificuldade.value,
+      ingrediente: inputIngredientes.value.split(','),
+      preparo: inputPreparo.value,
+      link: inputlink.value,
+      vegano: inputvegano.value,
   }
-
   console.log(novaReceita);
   listaDeReceitas.push(novaReceita);
+
   exibirReceitas();
 }
 
 function exibirReceitas() {
-  let htmlReceitas = '';
+let htmlReceitas = '';
+
   for (let index = 0; index < listaDeReceitas.length; index++) {
     htmlReceitas += `<article class="card">
     <h2>${listaDeReceitas[index].titulo}</h2>
-    <small>Dificuldade: ${listaDeReceitas[index].dificuldade} </small>
+    <small>Dificuldade: ${listaDeReceitas[index].dificuldade}</small>
     <p>${listaDeReceitas[index].preparo}</p>
-</article>`;
-  }
-
- let painelReceitas = document.querySelector('.painel-receitas');
- painelReceitas.innerHTML = htmlReceitas;
+    <p>${listaDeReceitas[index].link}</p>
+    <p>É vegano: ${ listaDeReceitas[index].vegano}</p>
+  </article>`;
 }
-exibirReceitas();
+  let painelReceitas = document.querySelector('.painel-receitas');
+  painelReceitas.innerHTML = htmlReceitas;
+}
 
-// exibirReceitas();
+ exibirReceitas();
 
-function deletarReceita(id) {
-  let novaListaDeReceitas = [];
+  function deletarReceita(id) {
+    let novaListaDeReceitas = [];
 
   for (let index = 0; index < listaDeReceitas.length; index++) {
     const receita = listaDeReceitas[index];
+
     if (receita.id != id) {
       novaListaDeReceitas.push(receita);
     }
   }
+
   if (novaListaDeReceitas.length == listaDeReceitas.length) {
     return console.log("Não encontrei o id");
   }
+
   listaDeReceitas = novaListaDeReceitas;
   console.log("receita deletada com sucesso!");
 }
 
-function deletarReceita(id) {
-  const novaListaDeReceitas = listaDeReceitas.filter(
+  function deletarReceita(id) {
+    const novaListaDeReceitas = listaDeReceitas.filter(
     (receita) => receita.id != id
   );
+
   if (novaListaDeReceitas.length == listaDeReceitas.length) {
     return console.log("Não encontrei o id");
   }
+
   listaDeReceitas = novaListaDeReceitas;
   console.log("receita deletada com sucesso!");
 }
+
   const condicaoDeReceita = (receita) => (receita.vegano = true);
   const atualizaReceita = (id, receitaAtualizada) => {
+
   let foiAtualizado = false;
 
   listaDeReceitas.forEach((receita) => {
     if (receita.id != id) {
       return;
     }
+
     if (receitaAtualizada.titulo) {
       receita.titulo = receitaAtualizada.titulo;
     }
+
     if (receitaAtualizada.ingredientes) {
       receita.ingredientes = receitaAtualizada.ingredientes;
     }
+
     foiAtualizado = true;
   });
 
@@ -130,20 +141,18 @@ function deletarReceita(id) {
     "Não foi encontrado o id"
   );
 };
+
+
   const buscarReceita = (termoBuscado) => {
-  const resultados = listaDeReceitas.filter(
+
+    const resultados = listaDeReceitas.filter(
     (receita) =>
     receita.titulo.toLowerCase().indexOf(termoBuscado.toLowerCase()) != -1
   );
 
-  // if (resultados.length) {
-  //   console.log(resultados);
-  // } else {
-  //   console.log("Não foi encontrado receitas");
-  // }
-
-  if (!resultados.length) {
+    if (!resultados.length) {
     console.log("Não foi encontrado receitas");
   }
-  return console.log(resultados);
+
+    return console.log(resultados);
 };
